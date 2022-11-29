94a4d1c1d64ffd2a1bd5e2e9efa8088788b8dbc7
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-11-29 10:48:21 -0500

    Second Exercise

--- Selection_Statement_Exercise/Program.cs
+++ Selection_Statement_Exercise/Program.cs
@@ -1,7 +1,8 @@
 ﻿//Selection Statement Exercise 
 //Exercise #1
 
-Console.WriteLine("This is exercise 1 of 2 the correct answer is 27");
+Console.WriteLine("This is exercise 1 of 2, to move forward, the correct answer is 27");
+Console.WriteLine();
 
 bool Correct = false;
 while (!Correct)
@@ -34,7 +35,34 @@ while (!Correct)
 //Exercise #2
 
 Console.WriteLine();
-Console.WriteLine();
 
 Console.WriteLine("What is your favorite school subjet?");
 var favSubject = Console.ReadLine();
+
+switch (favSubject.ToLower())
+{
+    case "history":
+        Console.WriteLine("History is amazing, it's my favorite too!") ;
+        break;
+
+    case "math":
+        Console.WriteLine("Math is a fun and complex subject!");
+        break;
+
+    case "english":
+        Console.WriteLine("A thine elegant choice!");
+        break;
+
+    case "science":
+        Console.WriteLine("The future is now thanks to Science!");
+        break;
+
+    case "music":
+        Console.WriteLine("The study of unseeable beauty!");
+        break;
+
+    default:
+        Console.WriteLine("What a great subject! Try a different subject for a more unique response!");
+        break;
+
+}
